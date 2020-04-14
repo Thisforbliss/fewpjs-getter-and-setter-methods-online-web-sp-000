e3ae3a511d5fa7dc9f536aa3ac692d7f79f464c8
Malcolm Leake <echelon1089@gmail.com>
2020-04-14 18:29:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Add your Circle class here
+class Circle {
+  constructor (radius, diameter, circumference){
+    this.radius= radius
+    this.diameter = radius * 2
+    this.circumference = circumference
+  }
+  
+}
